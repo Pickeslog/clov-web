516c234a4f7bee65642e3306c11fad9a5135def0
Myeongjun Kim <dora2156@naver.com>
2026-07-20 14:02:25 +0900

    feat: complete OAuth callback flow (#7) (#8)

--- src/router.jsx
+++ src/router.jsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, Navigate } from 'react-router-dom'
 import ProtectedRoute from './routes/ProtectedRoute'
 import Login from './pages/auth/Login/Login'
 import Signup from './pages/auth/Signup/Signup'
+import OAuthRedirect from './pages/auth/OAuthRedirect/OAuthRedirect'
 import Home from './pages/Home'
 
 // 라우팅 골격. 보호 라우트는 ProtectedRoute 하위에 둔다.
 export const router = createBrowserRouter([
   { path: '/login', element: <Login /> },
   { path: '/signup', element: <Signup /> },
+  { path: '/oauth2/redirect', element: <OAuthRedirect /> },
   {
     element: <ProtectedRoute />,
     children: [{ path: '/', element: <Home /> }],
